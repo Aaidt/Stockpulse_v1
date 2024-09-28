ba53647155328bd6b8272a373c8327cc469cceaa
Aaidt <141112198+Aaidt@users.noreply.github.com>
2024-09-28 13:31:15 +0530

    Delete stockpulseapp directory

--- stockpulseapp/forms.py
+++
@@ -1,6 +0,0 @@
-
-# stockpulseapp/forms.py
-from django import forms
-
-class StockForm(forms.Form):
-    stock_symbol = forms.CharField(label='Stock Symbol', max_length=10, widget=forms.TextInput(attrs={'placeholder': 'Enter stock symbol'}))
